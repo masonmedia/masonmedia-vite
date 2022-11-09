8aaa14327d79268c0936bf44e278338a12927d3b
Natalie <natalie@Alines-MacBook-Pro-5.local>
2022-11-09 16:19:31 -0500

    Update

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import Nav from './components/TheNav.vue'
 </script>
 
 <template>
-<div class="fade-in">
+<div>
   <header>
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
